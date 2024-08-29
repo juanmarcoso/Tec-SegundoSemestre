e8025d3a5cc01916696b5a634ace04ff7bc5a50b
Juan Marcos Orellana <juanmarcoso@gmail.com>
2024-08-28 21:46:56 -0300

    Agregando ejercicio4 de Java

---
+++ Java/Leccion03/EjercicioScanner04/src/Ciclos04/EjercicioJOption04.java
@@ -0,0 +1,25 @@
+/*
+Ejercicio 4: Pedir numeros hasta que se teclee uno negativo,
+y mostrar cuantos numeros se han introducido.
+ */
+package Ciclos04;
+
+import javax.swing.JOptionPane;
+
+public class EjercicioJOption04 {
+    
+    public static void main(String[] args) {
+        
+        int num;
+        int contador = 0;
+        
+        num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un numero: "));
+        
+        while (num >= 0) {
+            num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese otro numero: "));
+            contador++;
+        }
+        JOptionPane.showMessageDialog(null, "Usted digito " + contador + " numeros");
+    }
+    
+}
