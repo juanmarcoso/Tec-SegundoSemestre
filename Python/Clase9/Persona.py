df2091f08d1c21c6ef3836bf1abe16b2fd7fc5da
Juan Marcos Orellana <juanmarcoso@gmail.com>
2024-10-17 10:33:25 -0300

    Agregando clase 9 Python pt1.2

--- Python/Clase9/Persona.py
+++ Python/Clase9/Persona.py
@@ -4,12 +4,41 @@ class Persona:
         self.nombre = nombre
         self.edad = edad
         
-class Empleado(Persona): #Asi le asignamos que esta clase es hija de la clase Persona
+    @property
+    def nombre(self):
+        return self._nombre
+    
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
+    @property
+    def edad(self):
+        return self._edad
+    
+    @edad.setter
+    def edad(self, edad):
+        self._edad = edad
+        
+    def __str__(self): # Override = sobreescribir
+        return f'Persona: [Nombre: {self._nombre}. Edad: {self._edad}]'
+        
+class Empleado(Persona): #Asi le asignamos una clase hija a la clase padre
     
     def __init__(self, nombre, edad, sueldo):
-        
         super().__init__(nombre, edad)
-        self.sueldo = sueldo
+        self._sueldo = sueldo
+
+    @property
+    def sueldo(self):
+        return self._sueldo
+    
+    @sueldo.setter
+    def sueldo(self, sueldo):
+        self._sueldo = sueldo
+            
+    def __str__(self):
+        return f'Empleado: [Sueldo: {self._sueldo}] {super().__str__()}'
         
         
 empleado1 = Empleado('Juan', 32, 75000)
